a4af5bb5cd4466dc76b9a5184055de22e4aae8a8
devKitchen-cod <smagometov649@gmail.com>
2022-01-28 10:39:32 +0200

    updating frontend part

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,30 @@
-import { Button } from '@mui/material';
 import React from 'react';
+import Navbar from '../components/Navbar'
 
 const Index = () => {
-  return (
-    <>
-      <div className = "center">
-        <h1>Welcome!</h1>
-        <h3>Здесь собраны лучшие треки!</h3>      
-      </div>
-    </>
-  );
+    return (
+        <>
+        <Navbar/>
+            {/* <MainLayout> */}
+                <div className="center">
+                    <h1>Добро пожаловать!</h1>
+                    <h3>Здесь собраны лучшие треки!</h3>
+                </div>
+            {/* </MainLayout> */}
+
+            <style jsx>
+                {`
+                  .center {
+                    margin-top: 150px;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: center;
+                  }    
+                `}
+            </style>
+        </>
+    );
 };
 
 export default Index;
